999678e78a1b3390924fdd8853961616c80a80b5
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-09 16:56:42 +0000

    Solve addUpTo using  math formula which is faster than the example1

---
+++ big_O_notation/example2/addUpTo.js
@@ -0,0 +1,8 @@
+function addUpTo(n) {
+  return n * (n + 1) / 2
+}
+
+let t1 = performance.now();
+console.log(addUpTo(1000000000))
+let t2 = performance.now();
+console.log(`Time Elapse ${(t2 - t1) / 1000} seconds.`)
