271c01d1ac7ba2274dab520432baa3433ded8f07
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-09 16:54:53 +0000

    Use the big number to check the time complexity

--- big_O_notation/example1/addUpTo.js
+++ big_O_notation/example1/addUpTo.js
@@ -7,6 +7,6 @@ function addUpTo(num) {
 }
 
 let t1 = performance.now();
-console.log(addUpTo(6))
+console.log(addUpTo(1000000000))
 let t2 = performance.now();
 console.log(`Time Elapse ${(t2 - t1) / 1000} seconds.`)
