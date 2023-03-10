722588155af58a1acd25d324b2eca50c0d90ca0d
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-10 13:49:59 +0000

    Export the function

--- big_O_notation/example1/addUpTo.js
+++ big_O_notation/example1/addUpTo.js
@@ -10,3 +10,5 @@ let t1 = performance.now();
 console.log(addUpTo(1000000000))
 let t2 = performance.now();
 console.log(`Time Elapse ${(t2 - t1) / 1000} seconds.`)
+
+module.exports = addUpTo;
--- big_O_notation/example2/addUpTo.js
+++ big_O_notation/example2/addUpTo.js
@@ -3,6 +3,8 @@ function addUpTo(n) {
 }
 
 let t1 = performance.now();
-console.log(addUpTo(1000000000))
+console.log(addUpTo(100000))
 let t2 = performance.now();
 console.log(`Time Elapse ${(t2 - t1) / 1000} seconds.`)
+
+module.exports = addUpTo;
