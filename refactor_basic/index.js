df20d755873795f5169119572626207303caa2a5
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-11 07:33:25 +0000

    Initialize the directory and add the pseudocode of the result

---
+++ refactor_basic/index.js
@@ -0,0 +1,10 @@
+function charCount() {
+  // create an empty obj
+   // Create a for loop 
+  //  create a variable to make all characters in lowercase
+  // Check if there an empty space because we dont need to cout empty
+  // Check if the character is on the object
+  // Increment the character
+  // else to give a value of 1
+  // return the object
+}
