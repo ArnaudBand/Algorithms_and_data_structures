bec0f90215e82316ff364fc1adf10168aa173ec7
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-11 07:53:21 +0000

    Refactor the method

--- refactor_basic/index.js
+++ refactor_basic/index.js
@@ -1,17 +1,31 @@
-function charCount(str) {
-  let res = {};
-  for(let i = 0; i < str.length; i++) {
-    let char = str[i].toLowerCase();
+// function charCount(str) {
+//   let res = {};
+//   for(let i = 0; i < str.length; i++) {
+//     let char = str[i].toLowerCase();
 
+//     if(/[a-z0-9]/.test(char)) {
+//       if(res[char] > 0) {
+//         res[char]++;
+//       } else {
+//         res[char] = 1;
+//       }
+//     }
+//   }
+//   return res;
+// }
+
+// Refactor the code
+
+function charCount(str) {
+  let obj = {};
+
+  for (let char of str) {
+    char = char.toLowerCase();
     if(/[a-z0-9]/.test(char)) {
-      if(res[char] > 0) {
-        res[char]++;
-      } else {
-        res[char] = 1;
-      }
+      obj[char] = ++obj[char] || 1
     }
   }
-  return res;
+  return obj
 }
 
 module.exports = charCount;
