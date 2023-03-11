66f6d0de10f85a2b74e22b9419676707bdfe5248
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-11 08:19:07 +0000

    Use trim to implement the solution

--- refactor_basic/index.js
+++ refactor_basic/index.js
@@ -16,26 +16,39 @@
 
 // Refactor the code
 
-function isAlphaNumeric(char) {
-  let code = char.charCodeAt(0);
-  if(!(code > 47 && code < 58) && // numeric (0-9)
-     !(code > 64 && code < 91) && // upper alpha (A - Z)
-     !(code > 96 && code < 123)) {  // Lower alpha (a - z)}
-      return false;
-    }
-    return true;
-}
+// function isAlphaNumeric(char) {
+//   let code = char.charCodeAt(0);
+//   if(!(code > 47 && code < 58) && // numeric (0-9)
+//      !(code > 64 && code < 91) && // upper alpha (A - Z)
+//      !(code > 96 && code < 123)) {  // Lower alpha (a - z)}
+//       return false;
+//     }
+//     return true;
+// }
+
+// function charCount(str) {
+//   let obj = {};
+
+//   for (let char of str) {
+//     char = char.toLowerCase();
+//     if(isAlphaNumeric(char)) {
+//       obj[char] = ++obj[char] || 1
+//     }
+//   }
+//   return obj
+// }
 
 function charCount(str) {
-  let obj = {};
+  const result = {};
 
   for (let char of str) {
-    char = char.toLowerCase();
-    if(isAlphaNumeric(char)) {
-      obj[char] = ++obj[char] || 1
+    // ignore whitespace
+    if (char.trim() !== '') {
+      result[char] = result[char] ? result[char] + 1 : 1;
     }
   }
-  return obj
+
+  return result;
 }
 
 let t1 = performance.now();
