3f0ae2bb148a571bd9eabaa1b45d92014cdfe062
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-11 08:11:21 +0000

    Use the isAlphaNumeric method

--- refactor_basic/index.js
+++ refactor_basic/index.js
@@ -16,16 +16,31 @@
 
 // Refactor the code
 
+function isAlphaNumeric(char) {
+  let code = char.charCodeAt(0);
+  if(!(code > 47 && code < 58) && // numeric (0-9)
+     !(code > 64 && code < 91) && // upper alpha (A - Z)
+     !(code > 96 && code < 123)) {  // Lower alpha (a - z)}
+      return false;
+    }
+    return true;
+}
+
 function charCount(str) {
   let obj = {};
 
   for (let char of str) {
     char = char.toLowerCase();
-    if(/[a-z0-9]/.test(char)) {
+    if(isAlphaNumeric(char)) {
       obj[char] = ++obj[char] || 1
     }
   }
   return obj
 }
+
+let t1 = performance.now();
+console.log(charCount("Hi there, Count 1234"))
+let t2 = performance.now();
+console.log(`Time Elapse ${(t2 - t1) / 1000} seconds.`)
 
 module.exports = charCount;
