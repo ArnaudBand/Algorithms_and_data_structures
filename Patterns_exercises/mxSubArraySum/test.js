a31584a1e3d891db08b732e87c9e0e0ed3797c33
Arnaud Bandonkeye <ba104781@students.cavendish.ac.ug>
2023-04-20 10:58:34 +0000

    🐛 fix(test.js): fix typo in import statement for maxSubarraySum 🚀 perf(test.js): improve test coverage for negative numbers in maxSubarraySum

--- Patterns_exercises/mxSubArraySum/test.js
+++ Patterns_exercises/mxSubArraySum/test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const maxSubarraySum = require('./maxSubarraySum');
+const maxSubarraySum = require('./maxSubArraySum');
 
 describe('maxSubarraySum', function() {
   it('should return the maximum sum of n consecutive elements in the array', function() {
@@ -11,8 +11,8 @@ describe('maxSubarraySum', function() {
   });
 
   it('should handle negative numbers', function() {
-    assert.equal(maxSubarraySum([-6, -3, -2, -5, -1], 3), -6);
-    assert.equal(maxSubarraySum([-6, -3, -2, -5, -1], 4), -2);
+    assert.equal(maxSubarraySum([-6, -3, -2, -5, -1], 3), -8);
+    assert.equal(maxSubarraySum([-6, -3, -2, -5, -1], 4), -11);
     assert.equal(maxSubarraySum([2, -1, 4, -5, 1], 2), 3);
     assert.equal(maxSubarraySum([2, -1, 4, -5, 1], 3), 5);
   });
