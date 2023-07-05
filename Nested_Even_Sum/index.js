f4b634ef92e74990eaa95fecf4d3455105a655da
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-07-05 13:46:39 +0000

    fix the errors and test when I use even numbers

--- Nested_Even_Sum/index.js
+++ Nested_Even_Sum/index.js
@@ -2,10 +2,13 @@ const nestedEvenSum = (obj) => {
     let sum = 0;
 
     for(let key in obj) {
-        if(typeOf obj[key] === 'object' && obj[key] !== null) {
-            return sum+= nestedEvenSum(obj[key]); // recurcively call the nested object for the sum
-        } else if(typeOf obj[key] === 'number' && obj[key] % 2 === 0) {
+        if(typeof obj[key] === 'object' && obj[key] !== null) {
+            sum+= nestedEvenSum(obj[key]); // recurcively call the nested object for the sum
+        } else if(typeof obj[key] === 'number' && obj[key] % 2 === 0) {
             sum+= obj[key]; // Add the even numbers to the sum
         }
     }
+    return sum;
 }
+
+module.exports = nestedEvenSum;
---
+++ Nested_Even_Sum/test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const { describe, it } = require('mocha');
+
+const nestedEvenSum = require('./index');
+
+describe('nestedEvenSum', () => {
+    it('should return the correct sum of even numbers in a nested object', () => {
+        let obj = {
+            a: 2,
+            b: {
+              c: 4,
+              d: {
+                e: 6,
+                f: {
+                  g: 8
+                }
+              }
+            },
+            h: 10,
+          };
+          assert.strictEqual(nestedEvenSum(obj), 30);
+    }) 
+})
